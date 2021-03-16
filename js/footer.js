ca82e055938e6d78a76e9a180fa037942a15cc47
4life <povchinnikov@nwave.io>
2021-03-16 18:19:56 +0300

    Google analytics

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,7 @@
 document.querySelector('time').innerText = `Ноябрь 07, ${new Date().getFullYear()}`;
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-2B0JZYV657');
