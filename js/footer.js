254981e1e2fbccd8b13f62042e2270cbbbdd17d7
4life <pavel.ovchinnikov@reustle.co>
2022-03-11 12:48:44 +0300

    Fix scripts

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,26 @@
 const elements = Array.from(document.querySelectorAll('button.more'));
 const open = (el) => el.target.parentElement.classList.add('open');
-elements.forEach(el => el.addEventListener('click', open.bind(el)));
 
+function checkAnchor() {
+  const hash = window.location.hash.split('#')[1];
+  if (!hash) {
+    return;
+  }
+
+  const anchor = document.getElementById(hash);
+  if (anchor) {
+    anchor.parentElement.classList.add('open');
+  }
+}
+
+// google analytics
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-
 gtag('config', 'G-2B0JZYV657');
+
+checkAnchor();
+
+// events
+window.addEventListener('hashchange', () => checkAnchor(), false);
+elements.forEach(el => el.addEventListener('click', open.bind(el)));
